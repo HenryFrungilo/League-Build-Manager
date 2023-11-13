1f39d9022c16e9df6d38d2f53b82733bcaef37f6
Henry Alexandre de Almeida Frungilo <henryfrungilo@outlook.com>
2023-11-13 11:32:05 -0300

    Created Page Header and Introduction Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,19 @@ import { AppComponent } from './app.component';
 import { MainpageModule } from './pages/tft/mainpage/mainpage.module';
 import { PageNotFoundModule } from './pages/page-not-found/page-not-found.module';
 import { IntroductionPageModule } from './pages/introduction-page/introduction-page.module';
+import { PageHeaderComponent } from './pages/page-header/page-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MainpageModule,
-    PageNotFoundModule
+    PageNotFoundModule,
+    IntroductionPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
